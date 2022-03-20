69d56847ec0775a2a67c105a639cbe27a02a347c
장정원 <jellygarden617@gmail.com>
2022-03-20 19:10:20 +0900

    lesson4-1

--- fn.test.js
+++ fn.test.js
@@ -1,53 +1,27 @@
-const { getName } = require('./fn');
 const fn = require('./fn');
 
-/**
- *** callback
- **/
-// test('3초 뒤에 받아온 이름은 Mike', (done) => {
-//     function callback(name) {
-//         expect(name).toBe('Mike');
-//         // done();
-//     }
-//     fn.getName(callback);
-// });
-// test('3초 뒤에 받아온 이름은 Mike', (done) => {
-//     function callback(name) {
-//         try {
-//             expect(name).toBe('Mike');
-//             done();
-//         } catch (error) {
-//             done();
-//         }
-//     }
-//     fn.getName(callback);
-// });
+let num = 0;
 
+beforeEach(() => {
+    num = 0;    
+});
 
-/**
- *** Promise
- * resolves, rejects
- **/
-// test('3초 뒤에 받아온 나이는 30', () => {
-//     return fn.getAge().then(age => {
-//         expect(age).toBe(30);
-//     });
-// });
-// test('3초 뒤에 받아온 나이는 30', () => {
-//     return expect(fn.getAge()).resolves.toBe(30);
-// });
-// test('3초 뒤에 에러 발생', () => {
-//     return expect(fn.getAge()).rejects.toMatch('서버 에러..');
-// });
+test('0 + 1 = 1', () => {
+    num = fn.add(num, 1);
+    expect(num).toBe(1);
+});
 
+test('0 + 2 = 2', () => {
+    num = fn.add(num, 2);
+    expect(num).toBe(2);
+});
 
-/**
- * async, await
- **/
-// test('3초 뒤에 받아온 나이는 30', async () => {
-//     const age = await fn.getAge();
-//     expect(age).toBe(30);
-// });
-test('3초 뒤에 받아온 나이는 30', async () => {
-    await expect(fn.getAge()).resolves.toBe(30);
+test('0 + 3 = 3', () => {
+    num = fn.add(num, 3);
+    expect(num).toBe(3);
+});
+
+test('0 + 4 = 4', () => {
+    num = fn.add(num, 4);
+    expect(num).toBe(4);
 });
