44f1d5c6b9b64ae960c68e385329622bc0c0353d
장정원 <jellygarden617@gmail.com>
2022-03-20 19:35:36 +0900

    lesson4-4

--- fn.test.js
+++ fn.test.js
@@ -1,25 +1,23 @@
 const fn = require('./fn');
 
-let user;
-
-beforeAll(() => console.log('밖 beforeAll')); // 1
-beforeEach(() => console.log('밖 beforeEach')); // 2, 6
-afterEach(() => console.log('밖 afterEach')); // 4, 10
-afterAll(() => console.log('밖 afterAll')); // 마지막 
+let num = 0;
 
 test('0 + 1 = 1', () => {
-    console.log('밖 test');
-    expect(fn.add(0, 1)).toBe(1); // 3
+    expect(fn.add(num, 1)).toBe(1);
 });
-
-describe('Car 관련 작업', () => {
-    beforeAll(() => console.log(' 안 beforeAll')); // 5
-    beforeEach(() => console.log(' 안 beforeEach')); // 7
-    afterEach(() => console.log(' 안 afterEach')); // 9
-    afterAll(() => console.log(' 안 afterAll')); // 마지막 -1
-    
-    test('0 + 2 = 2', () => {
-        console.log('안 test');
-        expect(fn.add(0, 2)).toBe(2); // 8
-    });
+test('0 + 2 = 2', () => {
+    expect(fn.add(num, 2)).toBe(2);
 });
+test('0 + 3 = 3', () => {
+    expect(fn.add(num, 3)).toBe(3);
+});
+test.skip('0 + 4 = 4', () => {
+    expect(fn.add(num, 4)).toBe(4);
+    num = 10;
+});
+test('0 + 5 = 5', () => {
+    expect(fn.add(num, 5)).toBe(5);
+});
+// test.only('0 + 5 = 5', () => {
+//     expect(fn.add(num, 5)).toBe(5);
+// });
