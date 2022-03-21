e4c2cb94ed5ca79ac1f7d911b50c7d8f3b0f438d
장정원 <jellygarden617@gmail.com>
2022-03-22 06:44:32 +0900

    lesson5-4

--- fn.test.js
+++ fn.test.js
@@ -1,14 +1,17 @@
-const mockFn = jest.fn(num => num + 1);
+const mockFn = jest.fn();
 
-mockFn(10);
-mockFn(20);
-mockFn(30);
+mockFn
+.mockReturnValueOnce(10)
+.mockReturnValueOnce(20)
+.mockReturnValueOnce(30)
+.mockReturnValue(40)
+
+mockFn();
+mockFn();
+mockFn();
+mockFn();
 
 test('함수 호출은 3번 됩니다.', () => {
-    expect(mockFn.mock.calls.length).toBe(3);
-});
-test('리턴된 값은 11, 21, 31 입니다.', () => {
-    expect(mockFn.mock.results[0].value).toBe(11);
-    expect(mockFn.mock.results[1].value).toBe(21);
-    expect(mockFn.mock.results[2].value).toBe(31);
+    console.log(mockFn.mock.results);
+    expect(mockFn.mock.calls.length).toBe(4);
 });
